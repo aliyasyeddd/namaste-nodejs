92bfa2df052acf1dcf60bf428fbc664fea8a3bed
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-03-26 15:30:42 +0530

    Refactor server and update route/port
    Tidied up server.js: added explanatory comments, switched to an arrow-function request handler and standardized the require call. Renamed the endpoint from /getSecretData to /secretData (now returns 'This is secret data') and changed the listening port from 7777 to 7775. Also adjusted the default response text for clarity.

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const http = require("node:http");
+//node js has an module that module is known as http module
+//import http module
+//core module
+const http = require('node:http');
 
-const server = http.createServer(function (req, res) {
-  if (req.url === "/getSecretData") {
-    res.end("There is no secret data");
-  }
-  res.end("Hello World!");
-});
+//create a server
+//creating application which can listen to the request and send response to the client
+const server = http.createServer((req, res) => {
+    //req is the request object which contains all the information about the request
+    //res is the response object which is used to send the response to the client
+    
 
-server.listen(7777);
+    if(req.url === '/secretData') {
+        res.end('This is secret data')
+    }
+
+
+    //whoever coming to server i will send the response as hello world
+    res.end('Hello World') //sending the data back to the client
+    //replies the the request
+
+
+})
+
+//server is ready to listen to the request
+server.listen(7775)
