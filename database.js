7bf7c2e1d63e0d9b8b33e3b7d185fd538f1db868
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-03-26 19:53:25 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,47 +1,4 @@
-const { MongoClient } = require("mongodb");
 
-const url =
-  "mongodb+srv://namastedev:ECXK5j0d4OuHZhs6@namastenode.rc211ms.mongodb.net/";
-
-const client = new MongoClient(url);
-
-const dbName = "HelloWorld";
-
-async function main() {
-  await client.connect();
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  const collection = db.collection("User");
-
-  const data = {
-    firstname: "Ranveer",
-    lastname: "Singh",
-    city: "Mumbai",
-    phoneNumber: "987543210",
-  };
-
-  const insertResult = await collection.insertOne(data);
-  console.log("Inserted documents =>", insertResult);
-
-  // Read
-  const findResult = await collection.find({}).toArray();
-  console.log("Found documents =>", findResult);
-
-  const countResult = await collection.countDocuments({});
-  console.log("Count of documents in the User collection =>", countResult);
-
-  // Find all documents with a filter of firstname: Deepika
-
-  const result = await collection.find({ firstname: "Deepika" }).count();
-  console.log("result => ", result);
-
-  return "done.";
-}
-
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
 
 // NOTES
 
@@ -54,3 +11,5 @@ main()
 // INstall mongodb package
 // Create a connection from code
 // Documents CRUD - CReate, REad, Update, Delete
+
+const URI  = "mongodb+srv://syedaliya03_db_user:1135518Aa@nodelevelone.iwr3d25.mongodb.net/"
