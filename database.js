301afa701cbf3f276f546c3bffa559198b946fe6
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-03-29 19:35:52 +0530

    Add MongoDB client example and bump mongodb
    Add a standalone MongoDB client demo (database.js) that uses MongoClient to connect, insert a sample user, read documents, count them, and close the client. Also update package.json to bump the mongodb dependency to ^6.21.0. Note: the connection string still contains the <PASSWORD> placeholder.

--- database.js
+++ database.js
@@ -12,4 +12,48 @@
 // Create a connection from code
 // Documents CRUD - CReate, REad, Update, Delete
 
-const URI  = "mongodb+srv://syedaliya03_db_user:<PASSWORD>@nodelevelone.iwr3d25.mongodb.net/"
+const { MongoClient } = require("mongodb")
+
+const url = "mongodb+srv://syedaliya03_db_user:<PASSWORD>@nodelevelone.iwr3d25.mongodb.net/"
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'NodeJs'
+
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('User');
+
+
+  const data = {
+    firstname: 'faiza',
+    email: 'fzzz@gmail.com',
+    phoneNumber: '746781101'
+  }
+  
+
+  // inserting documents
+  const insertResult = await collection.insertMany([data]);
+  console.log('Inserted documents =>', insertResult);
+
+  //reading documents
+  const findResult = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult);
+
+  //counting documents
+  const countResult = await collection.countDocuments({});
+  console.log("Count of documents in the User collection =>", countResult);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
